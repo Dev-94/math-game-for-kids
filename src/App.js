a7812704a9243b19fb405afb7f232da690616b7e
Dev Govindji <friendsofdev94@gmail.com>
2020-03-18 16:06:01 -0500

    stubbs string into numbers algo

--- src/App.js
+++ src/App.js
@@ -2,8 +2,40 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  var [score, setScore] = useState(10);
+  const [question, newQuestion] = useState('Click Any Button!');
+  const [answer, newAnswer] = useState();
 
-  var [score, setScore] = useState(10)
+  const QA = [
+    {
+      question: '5+5', a: '2', b: '14', c: '10',
+    },
+    {
+      question: '15+5', a: '20', b: '4', c: '19',
+
+    }
+  ];
+  // how to turn str into int, operate, turn in to string
+  const numRegEx = /\d*/g;
+  var q = QA[1].question; // accesses question in dataset
+  console.log('q: ' + q);
+  console.log('regex: ' + numRegEx);
+  var found = q.match(numRegEx); // returns array of numbers from `question`
+  console.log(found); // logs full array
+  var first = found[0]; // first number in question is idx0 in matched arr
+  console.log(first);
+  var second = found[2]; // first number in question is idx2 in matched arr
+  console.log(second);
+  // find operator and return in array
+  var operator
+  // access operator from array
+  console.log('operator: ' + operator)
+
+  // first + operator + second
+
+
+
+  // compare if same as answer choice
 
   // when answer is correct 10 points are added
   // displayed on score board
@@ -20,15 +52,29 @@ function App() {
 
   // state => score
 
+
+  // does not work
+  // var newQ = Number.parseInt('15') + parseInt('5'); //20
+  // console.log(newQ);
+
   return (
     <div className="App">
 
       <div>Score: {score}</div>
-      <div>Question: </div>
+      <div>Question: {question} </div>
 
-      <input type="submit" value="A" onClick={() => setScore(score * 2)} />
-      <input type="submit" value="B" onClick={() => setScore(score--)} />
-      <input type="submit" value="C" onClick={() => setScore(score = 'C')} />
+      <input type="submit" value="A" onClick={(e) => {
+        e.preventDefault();
+        setScore(score * 2);
+      }} />
+      <input type="submit" value="B" onClick={(e) => {
+        e.preventDefault();
+        setScore(score--);
+      }} />
+      <input type="submit" value="C" onClick={(e) => {
+        e.preventDefault();
+        setScore(score = 14);
+      }} />
 
 
 
