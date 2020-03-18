7724157d328165fca60d8e09eb0b9914e199fb13
Dev Govindji <friendsofdev94@gmail.com>
2020-03-18 14:27:10 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div>Score: </div>
+      <div>Question: </div>
+
+      <input type="submit" value="A" />
+      <input type="submit" value="B" />
+      <input type="submit" value="C" />
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
