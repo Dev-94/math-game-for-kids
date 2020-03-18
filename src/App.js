1cea3fd48fcfabc821e3f864502dfb21983f61d3
Dev Govindji <friendsofdev94@gmail.com>
2020-03-18 14:32:14 -0500

    adds pseudocode

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  // when answer is correct 10 points are added
+  // displayed on score board
+
+  // question appears
+  // answers appear
+  // answers appear in each button
+
+  // array of objects
+  // objects has keys for Q, 3 As, correct A
+
+  // game logic => if submitted A === correct A, next Q
+  // if submitted A !== correct A, don't re-render
+
   return (
     <div className="App">
 
